4fd48997cd84aeac7d386f4dacef5e06d63db332
ZeriusTY <>
2018-02-03 20:30:21 +0000

    Add Core Game
    Just uploading the main game

---
+++ aa_-Replica/Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject pinPrefab;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            SpawnPin();
+        }
+    }
+
+    void SpawnPin()
+    {
+        Instantiate(pinPrefab, transform.position, transform.rotation);
+    }
+}
